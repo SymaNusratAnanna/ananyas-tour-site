2548c94da7c76bc15024855a0680a0872a0ed4db
SymaNusratAnanna <symanusrat158@gmail.com>
2022-04-17 03:58:17 +0600

    service responsive

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+
 const Service = ({service}) => {
     const {name,img,description,price } = service;
     return (
         <div>
-           <img src="{img}"  alt="" />
+           <img src={img}  alt="" />
            <h2>{name}</h2>
            <p>Price : {price}</p>
            <p><small>{description}</small></p>
+           <button>Book </button>
         </div>
     );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
+import './Services.css'
 
 const Services = () => {
 
@@ -11,13 +12,15 @@ const Services = () => {
     },[])
     return (
         <div>
-            <h2>services: {services.length}</h2>
-            {
+            <h1 className='services-title'>Our services: {services.length}</h1>
+           <div className='services-container'>
+           {
                 services.map(service=> <Service
                 key={service.id}
                 service={service}
                 ></Service>)
             }
+           </div>
         </div>
     );
 };
