6d9c5e4e98255c4cac9d99422909430e7d255b4a
SymaNusratAnanna <symanusrat158@gmail.com>
2022-04-17 23:45:26 +0600

    register added

--- src/Login/Register/Register.js
+++ src/Login/Register/Register.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Register = () => {
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
+
+
     const navigate = useNavigate();
 
     const navigateLogin = () =>{
@@ -11,6 +23,11 @@ const Register = () => {
 
     const handleRegister = event =>{
         event.preventDefault();
+        
+
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
     }
 
     return (
@@ -20,12 +37,12 @@ const Register = () => {
             <Form onSubmit={handleRegister}>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Name</Form.Label>
-    <Form.Control type="name" placeholder="Enter your name" />
+    <Form.Control type="text" name="name" placeholder="Enter your name" />
     
   </Form.Group>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" required />
+    <Form.Control type="email" name="email" placeholder="Enter email" required />
     <Form.Text className="text-muted">
       We'll never share your email with anyone else.
     </Form.Text>
@@ -33,7 +50,7 @@ const Register = () => {
 
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" required />
+    <Form.Control type="password" name = "password" placeholder="Password" required />
   </Form.Group>
   
   <Form.Group className="mb-3" controlId="formBasicCheckbox">
